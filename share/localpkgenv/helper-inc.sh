f1cb7ac0edb624609ebf113faf5fb969d74fea2a
Marco Elver <me@marcoelver.com>
2012-10-29 23:23:12 +0000

    Added short options for --reinstall, --keep-build-dir

--- share/localpkgenv/helper-inc.sh
+++ share/localpkgenv/helper-inc.sh
@@ -4,8 +4,21 @@
 #
 # Supported (tested with): bash, zsh
 #
-# Author: Marco Elver <me AT marcoelver.com>
-# Date: Wed  3 Oct 15:44:17 BST 2012
+# Copyright (C) 2012, Marco Elver <me AT marcoelver.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 # Easier to detect shell when sourced, rather than by locakpkgenv itself.
 if [[ -z "$LOCALPKGENV_SHELL" ]]; then
