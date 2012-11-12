7f2aaf05c47abefaafd3ce8624a85439a7789886
Marco Elver <me@marcoelver.com>
2012-11-12 03:27:33 +0000

    pkgix: Add po/ folder and template
    Signed-off-by: Marco Elver <me@marcoelver.com>

--- share/pkgix/helper-inc.sh
+++ share/pkgix/helper-inc.sh
@@ -4,7 +4,7 @@
 #
 # Supported (tested with): bash, zsh
 #
-# Copyright (C) 2012, Marco Elver <me AT marcoelver.com>
+# Copyright (C) 2012, Marco Elver <me@marcoelver.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
