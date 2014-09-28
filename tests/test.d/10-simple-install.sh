8591c326d469bff57713b176527385003de1c22e
Marco Elver <me@marcoelver.com>
2014-09-28 20:21:40 +0100

    tests: Make tests not depend on path to TEST_ROOT
    Signed-off-by: Marco Elver <me@marcoelver.com>

--- tests/test.d/10-simple-install.sh
+++ tests/test.d/10-simple-install.sh
@@ -18,4 +18,4 @@ test_comment "List installed packages."
 test_call $EXIT_OK pkgix -n list simple
 
 verify simple 573576b0da634f7b4737f501143bd44483439bb04483473070b4b1f6d5f1cd9b
-verify_log    fa0b42848fb5bbc53e39014feb4626fd98c4785df2d1329dec2b99d551836210
+verify_log    ab3db0d8943980ddd3cd6953506495e6cbbe9a38cb7d93ec647cca905d6ce4fa
